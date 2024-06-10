2a239c6fe5f1d987cfefa55a1ba895cfd99008cf
swapnatootikoora1 <12345678+swapnatootikoora1@users.noreply.github.com>
2024-06-10 18:34:22 +0100

    first draft created

--- src/test/scala/uk/gov/hmrc/perftests/nicc/NICCRequests.scala
+++ src/test/scala/uk/gov/hmrc/perftests/nicc/NICCRequests.scala
@@ -1,43 +1,39 @@
 /*
  * Copyright 2023 HM Revenue & Customs
  *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
  */
 
 package uk.gov.hmrc.perftests.nicc
 
 import io.gatling.core.Predef._
+import io.gatling.core.structure.ChainBuilder
 import io.gatling.http.Predef._
-import io.gatling.http.request.builder.HttpRequestBuilder
 import uk.gov.hmrc.performance.conf.ServicesConfiguration
 
 object NICCRequests extends ServicesConfiguration {
 
-  val bearerToken: String = readProperty("bearerToken", "${authBearerToken}")
+  val baseUrl: String     = baseUrlFor("nicc")
+  val postNiccUrl: String = "/nicc-json-service/nicc/v1/api/national-insurance/{nationalInsuranceNumber}/contributions-and-credits/from/{startTaxYear}/to/{endTaxYear}"
+  val nationalInsuranceNumber: String = "SS000200"
+  val startTaxYear: String   = "2019-10-01"
+  val endTaxYear: String     = "2023-10-31"
 
-  val baseUrl: String = baseUrlFor("nicc") + "/individuals/tax-free-childcare/payments"
+  def postNICC: ChainBuilder =
+    exec(
+      http("Get niContribution and niCredit for NI number, start tax year date and end tax year date")
+        .post(s"$baseUrl$postNiccUrl/$nationalInsuranceNumber?startDate=$startTaxYear&endDate=$endTaxYear")
+        .check(status.is(200))
+    )
 
-  lazy val authBaseUrl: String = baseUrlFor("auth-login-api")
-
-  lazy val authUrl: String = s"$authBaseUrl/government-gateway/session/login"
-  val baseUrl: String = baseUrlFor("example-frontend")
-  val route: String   = "/check-your-vat-flat-rate"
-
-  val navigateToHomePage: HttpRequestBuilder =
-    http("Navigate to Home Page")
-      .get(s"$baseUrl$route/vat-return-period")
-      .check(status.is(200))
-      .check(css("input[name=csrfToken]", "value").saveAs("csrfToken"))
-
-  val postVatReturnPeriod: HttpRequestBuilder =
-    http("Post VAT return Period")
-      .post(s"$baseUrl$route/vat-return-period": String)
-      .formParam("vatReturnPeriod", s"$${vatReturnPeriod}")
-      .formParam("csrfToken", s"$${csrfToken}")
-      .check(status.is(303))
-      .check(header("Location").is("/check-your-vat-flat-rate/turnover").saveAs("turnOverPage"))
-
-  val getTurnoverPage: HttpRequestBuilder =
-    http("Get Turnover Page")
-      .get(s"$baseUrl$${turnOverPage}": String)
-      .check(status.is(200))
 }
--- src/test/scala/uk/gov/hmrc/perftests/nicc/NICCSimulation.scala
+++ src/test/scala/uk/gov/hmrc/perftests/nicc/NICCSimulation.scala
@@ -1,20 +1,56 @@
 /*
- * Copyright 2023 HM Revenue & Customs
+ * Copyright 2024 HM Revenue & Customs
  *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
  */
 
-package uk.gov.hmrc.perftests.example
+package uk.gov.hmrc.perftests.nicc
 
-import uk.gov.hmrc.performance.simulation.PerformanceTestRunner
-import uk.gov.hmrc.perftests.example.ExampleRequests._
+import io.gatling.core.Predef._
+import io.gatling.core.structure.ScenarioBuilder
+import uk.gov.hmrc.performance.simulation.{Journey, JourneySetup}
+import uk.gov.hmrc.perftests.Scenarios
 
-class NICCSimulation extends PerformanceTestRunner {
+import scala.concurrent.duration._
 
-  setup("home-page", "Home Page") withRequests navigateToHomePage
+class NICCSimulation extends Simulation with JourneySetup {
 
-  setup("post-vat-return-period", "Post vat return period") withRequests postVatReturnPeriod
+  val scenarioDefinitions: Seq[ScenarioDefinition] =
+    Seq(
+      Scenarios.niccJourney(runSingleUserJourney)
+    )
 
-  setup("get-turnover-page", "Get turnover page") withRequests getTurnoverPage
+  println(s"Setting up simulation")
 
-  runSimulation()
+  if (runSingleUserJourney) {
+    println(s"'perftest.runSmokeTest' is set to true, ignoring all loads and running with only one user per journey!")
+    val injectedBuilders = scenarioDefinitions.map { scenarioDefinition =>
+      scenarioDefinition.builder.inject(atOnceUsers(1))
+    }
+
+    setUp(injectedBuilders: _*)
+      .assertions(global.failedRequests.count.is(0))
+  } else {
+    setUp(withInjectedLoad(scenarioDefinitions): _*)
+      .assertions(global.failedRequests.percent.lte(1))
+      .maxDuration(10 minutes)
+  }
+
 }
+
+case class ScenarioDefinition(builder: ScenarioBuilder, load: Double) extends Journey {
+  def this(scenarioBuilder: ScenarioBuilder) {
+    this(scenarioBuilder, 1.0)
+  }
+}
+
