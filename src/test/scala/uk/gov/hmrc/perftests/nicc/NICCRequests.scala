6c92c6112bf6c2c8b704124c435ef5af8d88a9df
swapnatootikoora1 <170416052+swapnatootikoora1@users.noreply.github.com>
2024-07-02 18:45:58 +0100

    request body added

--- src/test/scala/uk/gov/hmrc/perftests/nicc/NICCRequests.scala
+++ src/test/scala/uk/gov/hmrc/perftests/nicc/NICCRequests.scala
@@ -1,41 +1,42 @@
-package uk.gov.hmrc.perftests.nicc
+/*
+ * Copyright 2023 HM Revenue & Customs
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
 
-import java.util.UUID
+package uk.gov.hmrc.perftests.nicc
 
 import io.gatling.core.Predef._
 import io.gatling.http.Predef._
 import io.gatling.http.request.builder.HttpRequestBuilder
-import play.api.libs.json.Json
 import uk.gov.hmrc.performance.conf.ServicesConfiguration
 
 object NICCRequests extends ServicesConfiguration {
 
   val baseUrl: String = baseUrlFor("nicc")
+  val postNiccUrl: String = "/nicc-json-service/v1/api/national-insurance/{nationalInsuranceNumber}/contributions-and-credits/from/{startTaxYear}/to/{endTaxYear}"
+  val nationalInsuranceNumber: String = "BB000200B"
+  val startTaxYear: String = "2019"
+  val endTaxYear: String = "2023"
 
-  val niccRequestBody: String = Json.obj(
-    "dateOfBirth" -> "1981-08-24",
-    "nino" -> "BB000200B",
-    "startTaxYear" -> "2020",
-    "endTaxYear" -> "2024",
-  ).toString()
+  val niccRequestBody: String = "{  \"dateOfBirth\": \"1960-04-05\" }".stripMargin
 
-  val headers: Map[String, String] = Map(
-    HttpHeaderNames.ContentType -> HttpHeaderValues.ApplicationJson,
-    "CorrelationId" -> UUID.randomUUID().toString
-  )
 
-  val callnicc: HttpRequestBuilder =
-    http("Calls the nicc endpoint with payload")
-      .post(s"$baseUrl/calculation")
-      .body(StringBody(niccRequestBody))
-      .headers(headers)
-      .check(status.is(200))
+  def postNICC: HttpRequestBuilder =
 
-  val callFinalCalculation: HttpRequestBuilder =
-    http("Calls the calculation endpoint with an final calc payload")
-      .post(s"$baseUrl/calculation")
-      .body(StringBody(finalCalcRequestBody))
-      .headers(headers)
-      .check(status.is(201))
+      http("Get niContribution and niCredit for NI number, start tax year date and end tax year date")
+        .post(s"$baseUrl$postNiccUrl/$nationalInsuranceNumber?startDate=$startTaxYear&endDate=$endTaxYear")
+        .body(StringBody(niccRequestBody))
+        .check(status.is(200))
 
 }
