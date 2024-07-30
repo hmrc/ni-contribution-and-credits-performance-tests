dc6dd86ee112daf430ba8bb50f67068ce66bdc74
martin-adaramola <169259944+martin-adaramola@users.noreply.github.com>
2024-07-30 10:02:22 +0100

    added auth retrieval

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -3,7 +3,11 @@ import sbt._
 object Dependencies {
 
   val test = Seq(
-    "uk.gov.hmrc"          %% "performance-test-runner"   % "6.0.0"         % Test
-  )
+    "uk.gov.hmrc"       %% "performance-test-runner"   % "6.0.0",
+    "org.playframework" %% "play-ahc-ws-standalone"  % "3.0.5",
+    "org.playframework" %% "play-ws-standalone-json" % "3.0.5",
+    "org.apache.pekko"  %% "pekko-stream"            % "1.0.3",
+    "ch.qos.logback"     % "logback-classic"         % "1.5.6"
+  ).map(_ % Test)
 
 }
---
+++ src/test/scala/uk/gov/hmrc/perftests/nicc/AuthRetriever.scala
@@ -0,0 +1,77 @@
+/*
+ * Copyright 2024 HM Revenue & Customs
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package uk.gov.hmrc.perftests.nicc
+
+import io.gatling.http.HeaderNames.Authorization
+import org.apache.pekko.actor.ActorSystem
+import play.api.libs.json.Json
+import play.api.libs.ws.JsonBodyWritables.writeableOf_JsValue
+import play.api.libs.ws.ahc.StandaloneAhcWSClient
+import uk.gov.hmrc.performance.conf.ServicesConfiguration
+
+import java.util.UUID
+import scala.concurrent.Await
+import scala.concurrent.duration.DurationInt
+
+object AuthRetriever extends ServicesConfiguration {
+  private implicit val system: ActorSystem = ActorSystem()
+  val wsClient: StandaloneAhcWSClient = StandaloneAhcWSClient()
+
+  private val authApiBaseUrl: String = baseUrlFor("auth-login-api")
+  private val authApiUrl: String = s"$authApiBaseUrl/government-gateway/session/login"
+
+  def getAuthToken: String = {
+    val response = Await.result(
+      wsClient
+        .url(authApiUrl)
+        .post(
+          Json.obj(
+            "affinityGroup"       -> "Organisation",
+            "credentialStrength"  -> "strong",
+            "confidenceLevel"     -> 50,
+            "credentialRole"      -> "Admin",
+            "credId"              -> UUID.randomUUID().toString/*,
+            "enrolments"         -> Seq( //incorrect enrolment,
+              Json.obj(
+                "key"         -> "HMRC-CUS-ORG",
+                "identifiers" -> Seq(
+                  Json.obj(
+                    "key"   -> "EORINumber",
+                    "value" -> "GB12938122"
+                  )
+                ),
+                "state"       -> "Activated"
+              )
+            )*/
+          )
+        ),
+      5.seconds
+    )
+
+
+    println(s"\n\nThe response status is ${response.status} and the body says: ${response.body}. \nHeaders include: ${response.headers}\n\n")
+    require(response.status == 201, "Unable to create auth token")
+
+    response.headers
+      .getOrElse(Authorization.toString, Seq("unable to get auth header"))
+      .flatMap(_.split(","))
+      .find(_.startsWith("Bearer"))
+      .get
+
+  }
+
+}
