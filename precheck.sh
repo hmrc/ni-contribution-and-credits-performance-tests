a99a71788c5265db7947f9324e5c6b5cc8af2805
Eoghan-Spillane-Accenture <201935567+Eoghan-Spillane-Accenture@users.noreply.github.com>
2025-09-05 11:44:21 +0100

    DL-17260: Update performance-test-runner to 6.2.0.

---
+++ precheck.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+
+sbt clean Test/scalafmt test scalafmtCheckAll scalafmtSbt
--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -1,13 +1,13 @@
-import sbt._
+import sbt.*
 
 object Dependencies {
 
-  val test = Seq(
-    "uk.gov.hmrc"       %% "performance-test-runner" % "6.1.0",
-    "org.playframework" %% "play-ahc-ws-standalone"  % "3.0.5",
-    "org.playframework" %% "play-ws-standalone-json" % "3.0.5",
-    "org.apache.pekko"  %% "pekko-stream"            % "1.0.3",
-    "ch.qos.logback"     % "logback-classic"         % "1.5.6"
+  val test: Seq[ModuleID] = Seq(
+    "uk.gov.hmrc"       %% "performance-test-runner" % "6.2.0",
+    "org.playframework" %% "play-ahc-ws-standalone"  % "3.0.7",
+    "org.playframework" %% "play-ws-standalone-json" % "3.0.7",
+    "org.apache.pekko"  %% "pekko-stream"            % "1.2.0",
+    "ch.qos.logback"     % "logback-classic"         % "1.5.18"
   ).map(_ % Test)
 
 }
